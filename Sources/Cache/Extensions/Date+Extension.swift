cfdaddaa2d91f19191e47280f1eb6ad4835f6ebb
Gil Biren <gilbiren@gmail.com>
2020-01-31 00:03:03 +0100

    Set proper creation date

---
+++ Sources/Cache/Extensions/Date+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by Gil Biren on 31/01/2020.
+//
+
+import Foundation
+
+extension Date {
+    var millis: TimeInterval {
+        return self.timeIntervalSince1970
+    }
+}
